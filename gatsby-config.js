1bfb3dbad17f91b703a4262895339698d76403f5
nazanin-bayati <nazanin.e.bayati@gmail.com>
2021-03-31 15:58:03 +0430

    Hotjar has been added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,26 @@
-//module.exports = {
-//  plugins: [
-//    {
-//      resolve: `gatsby-plugin-google-gtag`,
-//      options: {
-//        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-//        trackingIds: [
-//          "G-LN2843MG43", // Google Analytics / GA
-//   ],
-//        // This object is used for configuration specific to this plugin
-//        pluginConfig: {
-//          // Puts tracking script in the head instead of the body
-//          head: true,
-//        },
-//      },
-//    },
-//  ],
-//}
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-LN2843MG43", // Google Analytics / GA
+   ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
+        resolve: `gatsby-plugin-hotjar`,
+        options: {
+          includeInDevelopment: true, // optional parameter to include script in development
+          id: 2324339,
+          sv: 6
+          }
+        },
+    ],
+}
