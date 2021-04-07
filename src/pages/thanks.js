e1379651d05149f0ae1fffda42b5b78d14a8afe6
nazanin-bayati <nazanin.e.bayati@gmail.com>
2021-04-07 19:09:35 +0430

    Form edition

---
+++ src/pages/thanks.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from '../components/layout'
+
+const Thanks = () => {
+    return (
+        <Layout>
+            <div class="thanks-text">
+                <p>
+                    Thanks for filling information
+                </p>
+            </div>
+        </Layout>
+    )
+}
+
+export default Thanks
