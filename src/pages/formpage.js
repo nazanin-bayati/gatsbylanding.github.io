6f1603a87cdb2c3930e341a86d969dc2fd67fdbe
nazanin-bayati <nazanin.e.bayati@gmail.com>
2021-04-07 19:41:48 +0430

    Check3

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -4,15 +4,7 @@ const FormPage = () => {
     return (
         <div>
             <form name="contacts" method="POST" data-netlify="true">
-                <p>
-                    <label>Your Name: <input type="text" name="name" /></label>
-                </p>
-                <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
+                <input name="name" placeholder="Your name" type="text"></input>
             </form>
         </div>
     )
