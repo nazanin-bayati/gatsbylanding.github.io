2c6eda71136b0b4b56eb9681244a904e53367588
nazanin-bayati <nazanin.e.bayati@gmail.com>
2021-04-07 19:45:46 +0430

    CHECK4

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -3,8 +3,9 @@ import React from "react"
 const FormPage = () => {
     return (
         <div>
-            <form name="contacts" method="POST" data-netlify="true">
+            <form name="conta" method="POST" data-netlify="true">
                 <input name="name" placeholder="Your name" type="text"></input>
+                <button>Send</button>
             </form>
         </div>
     )
