d5462190287ae24907fe2cd0716061db31bf6a91
nazanin-bayati <nazanin.e.bayati@gmail.com>
2021-04-07 19:50:20 +0430

    CH

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -5,6 +5,8 @@ const FormPage = () => {
         <div>
             <form name="conta" method="POST" data-netlify="true">
                 <input name="name" placeholder="Your name" type="text"></input>
+                <input name="email" placeholder="Your email" type="email"></input>
+
                 <button>Send</button>
             </form>
         </div>
