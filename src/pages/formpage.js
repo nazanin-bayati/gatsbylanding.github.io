bca97b0cd5e4f00c3670500ea604d2d328b79d73
nazanin-bayati <nazanin.e.bayati@gmail.com>
2021-04-07 19:33:34 +0430

    Check2

--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Layout from '../components/layout'
 
 const FormPage = () => {
     return (
@@ -10,15 +9,6 @@ const FormPage = () => {
                 </p>
                 <p>
                     <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <label>Your Role: <select name="role[]" multiple>
-                        <option value="leader">Leader</option>
-                        <option value="follower">Follower</option>
-                    </select></label>
-                </p>
-                <p>
-                    <label>Message: <textarea name="message"></textarea></label>
                 </p>
                 <p>
                     <button type="submit">Send</button>
